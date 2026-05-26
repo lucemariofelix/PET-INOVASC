29b98d296138969bd4cb7469db599843db315e2c
Jose Lucemario <jlucemariof@gmail.com>
2026-05-26 15:23:28 -0300

    Update mensagens.js

--- backend/src/routes/mensagens.js
+++ backend/src/routes/mensagens.js
@@ -4,6 +4,7 @@ async function rotasMensagens(fastify, options) {
   
   // Rota para o React solicitar o disparo da notificação via Evolution API
   fastify.post('/mensagens/enviar', mensagemController.enviarMensagem);
+  fastify.get('/api/whatsapp/status', mensagemController.checarStatusWhatsApp);
 
 }
 
