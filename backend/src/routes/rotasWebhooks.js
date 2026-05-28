3cd251e6e403d1a7e7e5a663d149c5f97506aa79
Jose Lucemario <jlucemariof@gmail.com>
2026-05-28 10:54:22 -0300

    Create rotasWebhooks.js

---
+++ backend/src/routes/rotasWebhooks.js
@@ -0,0 +1,8 @@
+const webhookController = require('../controllers/webhookController');
+
+async function rotasWebhooks(fastify, options) {
+  // ATENÇÃO: Esta rota é PÚBLICA (não usa token). A Evolution precisa conseguir acessar livremente.
+  fastify.post('/webhooks/evolution', webhookController.receberStatusEvolution);
+}
+
+module.exports = rotasWebhooks;
