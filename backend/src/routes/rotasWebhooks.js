b051644b0f6cabdc6e92e01719c5335eadd10dcb
lucemariofelix <jlucemariof@gmail.com>
2026-06-02 20:23:27 -0300

    update: rotasWebhooks.js

--- backend/src/routes/rotasWebhooks.js
+++ backend/src/routes/rotasWebhooks.js
@@ -1,8 +1,8 @@
-const webhookController = require('../controllers/webhookController');
+const webhookController = require("../controllers/webhookController");
 
 async function rotasWebhooks(fastify, options) {
   // ATENÇÃO: Esta rota é PÚBLICA (não usa token). A Evolution precisa conseguir acessar livremente.
-  fastify.post('/webhook/evolution', webhookController.receberStatusEvolution);
+  fastify.post("/webhooks/evolution", webhookController.receberStatusEvolution);
 }
 
 module.exports = rotasWebhooks;
